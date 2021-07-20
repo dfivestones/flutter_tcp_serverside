4ae86f4ef1360fc8c90273731295e1c19cb9d3e1
dawit byun <byundawit@gmail.com>
2021-07-20 15:55:55 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,214 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:get_ip/get_ip.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: 'test',
+        home: SocketServer(),
+      ),
+    );
+
+class SocketServer extends StatefulWidget {
+  @override
+  _SocketServerState createState() => _SocketServerState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _SocketServerState extends State<SocketServer> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  List<MessageItem> items = [];
+
+  String localIP = "";
+
+  ServerSocket? serverSocket;
+  Socket? clientSocket;
+  int port = 9000;
+
+  TextEditingController msgCon = TextEditingController();
+
+  @override
+  void dispose() {
+    stopServer();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    getIP();
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(title: Text("Socket Server")),
+      body: Column(
+        children: <Widget>[
+          ipInfoArea(),
+          messageListArea(),
+          submitArea(),
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  Widget ipInfoArea() {
+    return Card(
+      child: ListTile(
+        dense: true,
+        leading: Text("IP"),
+        title: Text(localIP),
+        trailing: RaisedButton(
+          child: Text((serverSocket == null) ? "Start" : "Stop"),
+          onPressed: (serverSocket == null) ? startServer : stopServer,
+        ),
+      ),
+    );
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Widget messageListArea() {
+    return Expanded(
+      child: ListView.builder(
+          reverse: true,
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            MessageItem item = items[index];
+            return Container(
+              alignment: (item.owner == localIP)
+                  ? Alignment.centerRight
+                  : Alignment.centerLeft,
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: (item.owner == localIP)
+                        ? Colors.blue[100]
+                        : Colors.grey[200]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      (item.owner == localIP) ? "Server" : "Client",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      item.content,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  Widget submitArea() {
+    return Card(
+      child: ListTile(
+        title: TextField(
+          controller: msgCon,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.send),
+          color: Colors.blue,
+          disabledColor: Colors.grey,
+          onPressed: (clientSocket != null) ? submitMessage : null,
+        ),
+      ),
+    );
+  }
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void getIP() async {
+    var ip = await GetIp.ipAddress;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      localIP = ip;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  void startServer() async {
+    print(serverSocket);
+    serverSocket =
+        await ServerSocket.bind(InternetAddress.anyIPv4, port, shared: true);
+    print(serverSocket);
+    serverSocket!.listen(handleClient);
+  }
+
+  void handleClient(Socket client) {
+    clientSocket = client;
+
+    showSnackBarWithKey(
+        "A new client has connected from ${clientSocket!.remoteAddress.address}:${clientSocket!.remotePort}");
+
+    clientSocket!.listen(
+      (onData) {
+        print(String.fromCharCodes(onData).trim());
+        setState(() {
+          items.insert(
+              0,
+              MessageItem(clientSocket!.remoteAddress.address,
+                  String.fromCharCodes(onData).trim()));
+        });
+      },
+      onError: (e) {
+        showSnackBarWithKey(e.toString());
+        disconnectClient();
+      },
+      onDone: () {
+        showSnackBarWithKey("Connection has terminated.");
+        disconnectClient();
+      },
     );
   }
+
+  void stopServer() {
+    disconnectClient();
+    serverSocket!.close();
+    setState(() {
+      serverSocket = null;
+    });
+  }
+
+  void disconnectClient() {
+    if (clientSocket != null) {
+      clientSocket!.close();
+      clientSocket!.destroy();
+    }
+
+    setState(() {
+      clientSocket = null;
+    });
+  }
+
+  void submitMessage() {
+    if (msgCon.text.isEmpty) return;
+    setState(() {
+      items.insert(0, MessageItem(localIP, msgCon.text));
+    });
+    sendMessage(msgCon.text);
+    msgCon.clear();
+  }
+
+  void sendMessage(String message) {
+    clientSocket!.write("$message\n");
+  }
+
+  showSnackBarWithKey(String message) {
+    scaffoldKey.currentState!
+      ..hideCurrentSnackBar()
+      ..showSnackBar(SnackBar(
+        content: Text(message),
+        action: SnackBarAction(
+          label: 'Done',
+          onPressed: () {},
+        ),
+      ));
+  }
+}
+
+class MessageItem {
+  String owner;
+  String content;
+
+  MessageItem(this.owner, this.content);
 }
